b3239de8eeb631dce4eb6b95ece02ea092c296de
ronnapatp <ronnapat.sri@gmail.com>
2021-10-04 09:28:48 +0700

    Delete href in a tag

--- components/nav.js
+++ components/nav.js
@@ -12,11 +12,11 @@ export default function Nav() {
       </a>
 
       <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">Pricing</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
-        <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
+        <li><a className="nav-link px-2 link-secondary">Home</a></li>
+        <li><a className="nav-link px-2 link-dark">Features</a></li>
+        <li><a className="nav-link px-2 link-dark">Pricing</a></li>
+        <li><a className="nav-link px-2 link-dark">FAQs</a></li>
+        <li><a className="nav-link px-2 link-dark">About</a></li>
       </ul>
 
       <div className="col-md-3 text-end">
