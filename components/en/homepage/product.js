90596b533ee90be0b67f06324dbafa9a8b63025a
ronnapatp <ronnapat.sri@gmail.com>
2021-10-04 10:53:59 +0700

    Update product

--- components/en/homepage/product.js
+++ components/en/homepage/product.js
@@ -5,8 +5,9 @@ import Image from 'next/image';
 
 export default function Product() {
   return (
-    <div className="container center">
-<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div className="container">
+        <br /><br />
+<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 center">
         <div className="col">
           <div>
             <Image src="/product/rice/kumirice1.jpeg" alt="product" width={350} height={225}/>
@@ -27,7 +28,7 @@ export default function Product() {
         </div>
         <div className="col">
         <div>
-            <Image src="/kumiprofile.jpg" alt="product" width={100} height={225}/>
+            <Image src="/product/sticker/Kumisticker1.jpeg" alt="product" width={350} height={225}/>
           </div>
           <div className="center">
             <h3>Kumi rice</h3>
@@ -35,6 +36,7 @@ export default function Product() {
           </div>
         </div>
         </div>
+        <br /><br /><hr />
     </div>
   );
 }
