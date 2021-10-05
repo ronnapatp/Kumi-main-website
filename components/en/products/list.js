679f5996f55ced6094b229963f97d739e81289db
ronnapatp <ronnapat.sri@gmail.com>
2021-10-05 11:06:23 +0700

    Change class to className

--- components/en/products/list.js
+++ components/en/products/list.js
@@ -4,41 +4,41 @@ import Image from 'next/image';
 
 export default function Nav() {
   return (
-    <div class="container">
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
+    <div className="container">
+<div className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
       <Image src="/kumiprofile.jpg" alt="kumi" width={350} height={350} />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
+  <div className="col">
+    <div className="card">
       <Image src="/kumiprofile.jpg" alt="kumi" width={350} height={350} />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
+  <div className="col">
+    <div className="card">
       <Image src="/kumiprofile.jpg" alt="kumi" width={350} height={350} />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
+  <div className="col">
+    <div className="card">
       <Image src="/kumiprofile.jpg" alt="kumi" width={350} height={350} />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
