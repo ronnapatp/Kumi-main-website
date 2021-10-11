b5f161b971943b6266bd056cce279013fb975b57
ronnapatp <ronnapat.sri@gmail.com>
2021-10-11 10:41:17 +0700

    Create faq page & link faq page

--- components/en/homepage/faq.js
+++ components/en/homepage/faq.js
@@ -8,7 +8,7 @@ export default function Faq() {
       <br /><br /><br />
       <div className="center">
           <h1>FAQ</h1>
-          <p>View all FAQ</p>
+          <Link href="/en/faq"><a className="text-decoration-none text-dark">View all FAQ &rarr;</a></Link>
       </div> <br /><br />
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div>
---
+++ pages/en/faq.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+
+
+import Nav from '../../components/en/faq/nav'
+
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>FAQ | Kumi</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://kumithebear.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Kumi the chubby bear" />
+        <meta property="og:description" content="Kumi the chubby bear" />
+        <meta name="twitter:title" content="Kumi the chubby bear" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+    <Nav />
+      
+
+
+
+
+    </div>
+  )
+}
