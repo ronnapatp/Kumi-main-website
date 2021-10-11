1666bea6a88d630d8a8641b77f4eb46696de2a0b
ronnapatp <ronnapat.sri@gmail.com>
2021-10-11 11:08:29 +0700

    Add privacy policy page

---
+++ pages/en/privacy.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+
+import Nav from '../../components/en/privacy/nav'
+import Header from '../../components/en/privacy/head'
+
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>FAQ | Kumi</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://kumithebear.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Kumi the chubby bear" />
+        <meta property="og:description" content="Kumi the chubby bear" />
+        <meta name="twitter:title" content="Kumi the chubby bear" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+    <Nav />
+
+    <Header />
+      
+
+
+
+
+    </div>
+  )
+}
