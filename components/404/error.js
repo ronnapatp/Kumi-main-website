79c6ace89f37015492e476a921f4ba183226a139
ronnapatp <ronnapat.sri@gmail.com>
2021-10-14 11:36:17 +0700

    Add 404 page

---
+++ components/404/error.js
@@ -0,0 +1,40 @@
+import Img from 'next/image';
+import Link from 'next/link';
+
+export default function Error () {
+    return (
+        <div className="container">
+            <div className="px-4 py-5 my-5 text-center">
+                <span className="d-block mx-auto mb-4">
+                    <Img src="/kumiprofile.jpg" alt="kumi" width={72} height={72} />
+                </span>
+              <h1 className="display-5 fw-bold">404 - Page not found</h1>
+              <div className="col-lg-6 mx-auto">
+                <p>สำหรับภาษาไทยกรุณาเลื่อนลง</p>
+                <p className="lead mb-4">Kumi the chubby bear website can not find this page for you! But not worry you can back to home page by click button down below.</p>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <Link href="/">
+                  <button type="button" className="btn btn-outline-secondary btn-lg px-4">Back to home page!</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <br /><hr /><br />
+            <div className="px-4 py-5 my-5 text-center">
+                <span className="d-block mx-auto mb-4">
+                    <Img src="/kumiprofile.jpg" alt="kumi" width={72} height={72} />
+                </span>
+              <h1 className="display-5 fw-bold">404 - ไม่พบหน้านี้</h1>
+              <div className="col-lg-6 mx-auto">
+                <p>For English please sroll up</p>
+                <p className="lead mb-4">Kumi เว็บไซต์ไม่พบหน้านี้สําหรับคุณ! แต่ไม่ต้องกังวลคุณสามารถกลับไปที่หน้าแรกได้โดยคลิกปุ่มด้านล่าง</p>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <Link href="/th/">
+                  <button type="button" className="btn btn-outline-secondary btn-lg px-4">กลับหน้าหลัก!</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Nav from '../components/404/nav'
+import Error from '../components/404/error'
+import Footer from '../components/404/footer'
+
+export default function Custom404() {
+    return (
+        <div>
+
+            <Nav />
+
+            <Error />   
+
+            <Footer />
+        </div>
+    )
+}
