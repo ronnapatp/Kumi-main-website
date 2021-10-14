083e6cbfa63128bb8dc96c42a4045e76a99aaec1
ronnapatp <ronnapat.sri@gmail.com>
2021-10-14 11:39:44 +0700

    Add meta tag in 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Nav from '../components/404/nav'
 import Error from '../components/404/error'
 import Footer from '../components/404/footer'
@@ -5,6 +7,15 @@ import Footer from '../components/404/footer'
 export default function Custom404() {
     return (
         <div>
+          <Head>
+        <title>404 - Page not found</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://kumithebear.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Sorry we can't find this page for you" />
+        <meta name="twitter:title" content="Kumi the chubby bear" />
+        <meta name="twitter:card" content="summary_large_image" />
+            </Head>
 
             <Nav />
 
