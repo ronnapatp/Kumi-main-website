1593ccdc0a5f851de8e9be83cec34a67a7c6702f
ronnapatp <ronnapat.sri@gmail.com>
2021-10-14 10:53:31 +0700

    Translate from thai to en in about (thai page)

--- components/th/about/about.js
+++ components/th/about/about.js
@@ -7,9 +7,9 @@ export default function Nav() {
    <div className="container">
   <div className="row justify-content-center">
     <div className="col-4">
-      <h2>About us</h2>
+      <h2>เกี่ยวกับเรา</h2>
       <p>
-          Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+      แบรนด์ Kumi ก่อตั้งและเปิดตัวผลิตภัณฑ์ครั้งแรกเมื่อเดือนสิงหาคม 2562 ด้วยความร่วมมือ ระหว่างกุมารแพทย์โรคภูมิแพ้ และนักเคมีที่ต้องการพัฒนาผลิตภัณฑ์กันยุงที่มาจากธรรมชาติ อ่อนโยน สามารถใช้กับเด็กอ่อนได้ ในขณะที่คงประสิทธิภาพกันยุงได้ดีจึงร่วมมือกันคิดค้นและพัฒนาสูตร Kumi Ricebutter Balm ขึ้น โดยเลือกสรรส่วนผสมจากธรรมชาติ เช่น ไรซ์บัตเตอร์ หอมแดง ฯ และ ปราศจากสารอันตรายต่างๆ
       </p>
     </div>
     <div className="col-4">
