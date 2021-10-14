51796a2b1890e9f163da9beaa1215cf72009c130
ronnapatp <ronnapat.sri@gmail.com>
2021-10-14 10:33:34 +0700

    Translate from thai to en in faq (thai page)

--- components/th/homepage/faq.js
+++ components/th/homepage/faq.js
@@ -7,33 +7,33 @@ export default function Faq() {
   <div className="container">
       <br /><br /><br />
       <div className="center">
-          <h1>FAQ</h1>
-          <Link href="/en/faq"><a className="text-decoration-none text-dark">View all FAQ &rarr;</a></Link>
+          <h1>คำถามที่พบบ่อย</h1>
+          <Link href="/th/faq"><a className="text-decoration-none text-dark">ดูคำถามที่พบบ่อยทั้งหมด &rarr;</a></Link>
       </div> <br /><br />
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
             <div>
-                <h2>What is kumi?</h2>
-                <p>Kumi is bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+                <h2>อะไรคือคุมิ?</h2>
+                <p>คุมิคือ bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
             </div>
       </div>
       <br /><br />
