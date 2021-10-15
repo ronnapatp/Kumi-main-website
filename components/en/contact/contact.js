cdd0ba6c9fc2fde9607db6942eb1c5a2af8c3246
ronnapatp <ronnapat.sri@gmail.com>
2021-10-15 13:00:17 +0700

    Change map from th lang to en

--- components/en/contact/contact.js
+++ components/en/contact/contact.js
@@ -7,8 +7,7 @@ export default function Contact() {
    <div className="container">
   <div className="row justify-content-center">
     <div className="col-5">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.0922410983308!2d100.51577036460203!3d13.737150412465095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29927bbcec325%3A0x7c29f70cef77a152!2zMzIwIOC4luC4meC4meC4nuC4o-C4sOC4o-C4suC4oeC4l-C4teC5iCDguZQg4LmB4LiC4Lin4LiHIOC4oeC4q-C4suC4nuC4pOC4kuC4suC4o-C4suC4oSDguYDguILguJXguJrguLLguIfguKPguLHguIEg4LiB4Lij4Li44LiH4LmA4LiX4Lie4Lih4Lir4Liy4LiZ4LiE4LijIDEwNTAw!5e0!3m2!1sth!2sth!4v1634269278127!5m2!1sth!2sth" width="500" height="500" ></iframe>  
-    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31005.382445530322!2d100.5123078!3d13.7382486!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29927bbcec325%3A0x7c29f70cef77a152!2s320%20Rama%20IV%20Rd%2C%20Khwaeng%20Maha%20Phruttharam%2C%20Khet%20Bang%20Rak%2C%20Krung%20Thep%20Maha%20Nakhon%2010500!5e0!3m2!1sen!2sth!4v1634277562207!5m2!1sen!2sth" width="500" height="500"></iframe>    </div>
     <div className="col-5">
 
     <h2>Contact us</h2>
