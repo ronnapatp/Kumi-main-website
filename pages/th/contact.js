25510eb5be855d23351edab941ff9bda41fe2c54
ronnapatp <ronnapat.sri@gmail.com>
2021-10-15 10:58:13 +0700

    Update title

--- pages/th/contact.js
+++ pages/th/contact.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Contact | Kumi</title>
+        <title>ติดต่อเรา | คุมิ</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:url" content="https://kumithebear.vercel.app/" />
         <meta property="og:type" content="website" />
