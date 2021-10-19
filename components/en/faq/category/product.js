2105a3af6e102404fb4fd00ad13146a3ccde254e
ronnapatp <ronnapat.sri@gmail.com>
2021-10-19 12:14:47 +0700

    Change from class to className

--- components/en/faq/category/product.js
+++ components/en/faq/category/product.js
@@ -4,26 +4,26 @@ export default function Product() {
     return(
         <div id="product">
 <section>
-  <h1 class="text-center mb-4 pb-2 text-dark fw-bold">Product questions</h1>
+  <h1 className="text-center mb-4 pb-2 text-dark fw-bold">Product questions</h1>
 
 
-  <div class="row">
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5">What is kumi?</h6>
+  <div className="row">
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5">What is kumi?</h6>
       <p>
         Kumi is a bla bla bla bla bla bla bla bla blab lba lab alb alba blalbal balbla bla blablabl
       </p>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5">Who is the founder of kumi?</h6>
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5">Who is the founder of kumi?</h6>
       <p>
         You can see founder and history of Kumi the chubby bear at <Link href="/en/about"><a className="text-dark">About us</a></Link> page.
       </p>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5"><i class="fas fa-user text-dark fw-bolder fs-5 pe-2"></i> A simple
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5"><i className="fas fa-user text-dark fw-bolder fs-5 pe-2"></i> A simple
         question?
       </h6>
       <p>
@@ -32,8 +32,8 @@ export default function Product() {
       </p>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5"><i class="fas fa-rocket text-dark fw-bolder fs-5 pe-2"></i> A simple
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5"><i className="fas fa-rocket text-dark fw-bolder fs-5 pe-2"></i> A simple
         question?
       </h6>
       <p>
@@ -41,16 +41,16 @@ export default function Product() {
       </p>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5">A simple
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5">A simple
         question?
       </h6>
       <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
         reason.</p>
     </div>
 
-    <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-dark fw-bolder fs-5">Another
+    <div className="col-md-6 col-lg-4 mb-4">
+      <h6 className="mb-3 text-dark fw-bolder fs-5">Another
         question that is longer than usual</h6>
       <p>
         Of course! We’re happy to offer a free plan to anyone who wants to try our service.
